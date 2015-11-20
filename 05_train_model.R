e1d98e05d5a3a313d96f9cf8ca2b2e6edf79efc5
NikolaiKummer <nikolai.kummer@gmail.com>
2015-11-19 20:36:33 -0700

    Progress Update Thursday

--- 05_train_model.R
+++ 05_train_model.R
@@ -6,20 +6,50 @@ load(file="DAT_train_data_main.Rda")
 library("e1071")
 dfms$same=factor(dfms$same)
 
-n_examples=8000
-n_features=2000
+n_examples=3000
+n_features=1000
 
 hist_diff=as.data.frame(hist_diff)
 hist_diff$same=dfms$same
 
-hds=hist_diff[1:n_examples, c(1:n_features,2001)]
-hdp=hist_diff[6000:6100, c(1:n_features,2001)]
+hds=hist_diff[1:n_examples, c(1:n_features,1001)]
+hdp=hist_diff[6000:6100, c(1:n_features,1001)]
 
+#Model#1
 start <- Sys.time ()
-model <- svm(same~., data=hds,kernel="linear")
+model1 <- svm(same~., data=hds,kernel="linear")
 print(Sys.time () - start)
-
-p=predict(model,newdata = hdp )
-
+p=predict(model1,newdata = hdp )
 sum(p==hdp$same)/nrow(hdp)*100
-table(pred=p, true=hdp$same)
+res1=table(pred=p, true=hdp$same)
+save(model1,res1, file="DAT_model1.Rda")
+
+#Model#2
+start <- Sys.time ()
+model2 <- svm(same~., data=hds,kernel="linear", cost=0.001)
+print(Sys.time () - start)
+p=predict(model2,newdata = hdp )
+sum(p==hdp$same)/nrow(hdp)*100
+res2=table(pred=p, true=hdp$same)
+save(model2,res2, file="DAT_model2.Rda")
+
+#Model#3
+start <- Sys.time ()
+model3 <- svm(same~., data=hds,kernel="linear", cost=100)
+print(Sys.time () - start)
+t3=Sys.time () - start
+p=predict(model3,newdata = hdp )
+sum(p==hdp$same)/nrow(hdp)*100
+res3=table(pred=p, true=hdp$same)
+save(model3,res3, t3, file="DAT_model3.Rda")
+
+
+#Model#4
+start <- Sys.time ()
+model4 <- svm(same~., data=hds)
+print(Sys.time () - start)
+t4=Sys.time () - start
+p=predict(model4,newdata = hdp )
+sum(p==hdp$same)/nrow(hdp)*100
+res4=table(pred=p, true=hdp$same)
+save(model4,res4,t4, file="DAT_model4.Rda")
